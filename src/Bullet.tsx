d0ab31897e855065066ead743709e13c8b618994
Jesse Szepieniec <jessems@gmail.com>
2023-02-16 17:56:38 +0100

    Brought triangle on same line as text

--- src/Bullet.tsx
+++ src/Bullet.tsx
@@ -4,44 +4,47 @@ import Triangle from './Triangle';
 
 const Bullet = ({ data }: { data: Array<any> }) => {
 	const [showContents, setShowContents] = React.useState(true);
-	// const handleClick = () => {
-	// 	setShowContents(!showContents);
-	// 	console.log(showContents);
-	// };
-
 	return (
-		<div style={{ paddingLeft: '20px', position: 'relative' }}>
-			<Triangle
-				style={{
-					position: 'absolute',
-					left: '-4px',
-					transform: showContents ? 'rotate(90deg)' : 'rotate(0deg)'
-				}}
-				onClick={() => setShowContents(!showContents)}
-			/>
+		<>
 			{data.map(parent => {
 				return (
-					<div key={parent.name}>
-						<span
-							contentEditable={true}
-							suppressContentEditableWarning={true}
-						>
-							{parent.name}
-						</span>
-						{/* Base Condition and Rendering recursive component from inside itself */}
-						<div
+					<div
+						style={{
+							paddingLeft: '20px',
+							position: 'relative',
+							display: 'flex'
+						}}
+					>
+						<Triangle
 							style={{
-								display: showContents ? 'block' : 'none'
+								transform: showContents
+									? 'rotate(90deg)'
+									: 'rotate(0deg)'
 							}}
-						>
-							{parent.children && (
-								<Bullet data={parent.children} />
-							)}
+							onClick={() => setShowContents(!showContents)}
+						/>
+						<div key={parent.name}>
+							<span
+								contentEditable={true}
+								suppressContentEditableWarning={true}
+							>
+								{parent.name}
+							</span>
+							{/* Base Condition and Rendering recursive component from inside itself */}
+							<div
+								style={{
+									display: showContents ? 'block' : 'none'
+								}}
+							>
+								{parent.children && (
+									<Bullet data={parent.children} />
+								)}
+							</div>
 						</div>
 					</div>
 				);
 			})}
-		</div>
+		</>
 	);
 };
 
